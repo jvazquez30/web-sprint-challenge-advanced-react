4992cf41eb6b74bc0981cb43ef9bb690da603dc7
jvazquez30 <josevazquez881@gmail.com>
2024-05-21 12:54:17 -0700

    answer added to it and npm usertest

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -3,7 +3,7 @@ import React from "react"
 import AppFunctional from "./AppFunctional"
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen, waitFor } from '@testing-library/react'
-import { userEvent } from '@testing-library/user-event'
+import  userEvent from '@testing-library/user-event'
 import server from "../../backend/mock-server"
 
 describe('Extra 5 tests', () => {
